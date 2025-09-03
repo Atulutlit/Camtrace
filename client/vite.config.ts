fb0ac08819a37928142c2f5b2186694e87550be5
root <root@srv969466.hstgr.cloud>
2025-09-03 16:05:21 +0000

    vite config added

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+   server: {
+    host: true, // allows network access
+    port: 5173,
+    allowedHosts: [
+      'ebharatsec.in',
+      'www.ebharatsec.in',
+      '31.97.237.71' // optional, if accessing via IP
+    ]
+  }
 })
